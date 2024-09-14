2744f6a8be5601865e2d9e5a328ba5f9274d8f3a
Onur <cetineronur44@gmail.com>
2024-09-14 23:07:11 +0200

    14.09.2024 apitest

--- src/test/java/tester_productprocess_ts/utilities/apiUtilities/Authentication.java
+++ src/test/java/tester_productprocess_ts/utilities/apiUtilities/Authentication.java
@@ -1,19 +1,30 @@
 package tester_productprocess_ts.utilities.apiUtilities;
 
+import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import tester_productprocess_ts.stepDefinitions.apiStepDefinitions.GetRequest;
 
 import static io.restassured.RestAssured.given;
-public class Authentication {
-    public static String token(String email, String password){
-        String url = "";
-        //String body = "{"email": ""+email+"","password": ""+password+""}";
 
-        return given()
+public class Authentication  {
+
+    String token;
+    Response response;
+
+    public String token(){
+
+        RestAssured.baseURI = "http://localhost:8080";
+
+        response = given()
                 .when()
-               // .body(body)
                 .contentType(ContentType.JSON)
-                .post(url)
-                .jsonPath()
-                .getString("token");
+                .body("{ \"username\": \"UretimPlanlama\", \"password\": \"Ankara04*\" }")
+                .when()
+                .post("/auth/login");
+
+        // Token'ı yanıt içinden alıp geri döndürüyoruz
+        return token = response.jsonPath().getString("token");
+
     }
 }
---
+++ src/test/resources/features/api_features/api.feature
@@ -0,0 +1,4 @@
+Feature: Api Test
+  Scenario:
+    And idsi 87 olan siparis Apiden test edilir
+    And bir siparis eklenir ve Apiden test edilir
